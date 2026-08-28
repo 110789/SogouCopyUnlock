9a0d5a6c9f1471f4248eff3a80dc4f130b3a4fac
110789 <110789@users.noreply.github.com>
2026-08-28 17:42:59 +0800

    fix: 提示气泡不是系统Toast，改为拦截TextView.setText精确匹配

--- app/src/main/java/com/ayou/sogoucopyunlock/HookEntry.java
+++ app/src/main/java/com/ayou/sogoucopyunlock/HookEntry.java
@@ -1,9 +1,6 @@
 package com.ayou.sogoucopyunlock;
 
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -46,38 +43,24 @@ public class HookEntry implements IXposedHookLoadPackage {
                     }
                 });
 
-        XposedHelpers.findAndHookMethod(Toast.class, "show", new XC_MethodHook() {
+        XC_MethodHook textHook = new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) {
-                Toast toast = (Toast) param.thisObject;
-                View view = toast.getView();
-                if (view == null) {
+                Object arg = param.args[0];
+                if (arg == null) {
                     return;
                 }
-                String text = findText(view);
-                if (TOAST_TEXT.equals(text)) {
-                    log("suppressed copy-limit toast");
-                    param.setResult(null);
+                if (TOAST_TEXT.contentEquals((CharSequence) arg)) {
+                    log("suppressed copy-limit tip view");
+                    param.args[0] = "";
                 }
             }
-        });
-    }
+        };
 
-    private static String findText(View view) {
-        if (view instanceof TextView) {
-            CharSequence t = ((TextView) view).getText();
-            return t == null ? null : t.toString();
-        }
-        if (view instanceof ViewGroup) {
-            ViewGroup group = (ViewGroup) view;
-            for (int i = 0; i < group.getChildCount(); i++) {
-                String found = findText(group.getChildAt(i));
-                if (found != null) {
-                    return found;
-                }
-            }
-        }
-        return null;
+        XposedHelpers.findAndHookMethod(TextView.class, "setText",
+                CharSequence.class, textHook);
+        XposedHelpers.findAndHookMethod(TextView.class, "setText",
+                CharSequence.class, TextView.BufferType.class, textHook);
     }
 
     private static boolean isCalledFromClipboardGuard() {
